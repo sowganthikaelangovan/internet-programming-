e49dbc9e1af8ef4aeab6f67ba604c14af99a743b
sowganthikaelangovan <sowganthikae1071.sse@saveetha.com>
2025-07-09 10:29:02 +0530

    Delete Exp 6 directory

--- Exp 6/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "cake_db");
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
--- Exp 6/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connection.php");
-if (isset($_GET['id'])) {
-  $id = intval($_GET['id']);
-  $conn->query("DELETE FROM orders WHERE id = $id");
-}
-header("Location: view.php");
-?>
