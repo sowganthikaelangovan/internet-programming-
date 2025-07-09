e89b04cc790af0941aab415fab992540c294e673
sowganthikaelangovan <sowganthikae1071.sse@saveetha.com>
2025-07-09 10:29:11 +0530

    Delete Exp 7 directory

--- Exp 7/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connection.php");
-if (isset($_GET['id'])) {
-  $id = intval($_GET['id']);
-  $conn->query("DELETE FROM appointments WHERE id = $id");
-}
-header("Location: appointments.php");
-?>
